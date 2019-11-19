cad83a3500e37870ffffba4c2e895b4da3e14128
dinub <dinuflow@outlook.com>
2019-11-19 00:36:59 -0300

    new menus in JSs

--- src/navbar.js
+++ src/navbar.js
@@ -1,3 +1,10 @@
 var navbar = document.getElementById('navbar');
 
 navbar.innerHTML += ` <h2 class="name">DinuB</h2>`;
+
+var menus = ['Home', 'Bio', 'Portofolio'];
+
+for (value of menus){
+  navbar.innerHTML +=
+  `<a href="#" class="menuLink">${value}</a>`
+}
