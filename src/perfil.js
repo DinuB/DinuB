ab1e1e87f71fc45952fcfb1b23ab835e6fb0f88e
dinub <dinuflow@outlook.com>
2019-11-26 18:02:04 -0300

    ligar com API do GitHub

--- src/perfil.js
+++ src/perfil.js
@@ -1,24 +1,48 @@
-var perfil = {
-  nome: 'Dinu Buzut',
-  foto: 'dinubuzut.png',
-  perfil: [
-    'Team Leader',
-    'UX / UI',
-    'Design',
-    'Front End Developer',
-    'Network',
-    'Security',
-    'Helpdesk'
-  ]
+var json;
+var submit;
+
+function submit(){
+  submit = document.getElementById('input').value;
 }
 
-document.getElementById('perfil').innerHTML =
-`
-  <div id="foto">
-    <img src="src/img/${perfil.foto}" alt="">
-  </div>
-  <div id="info">
-    <h3>${perfil.nome}</h3>
-    <p>${perfil.perfil.join('<br>')}</p>
-  </div>
-`;
+var minhaPromise = function(){
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/dinub');
+        // xhr.open('GET', 'https://api.linkedin.com/v1/dinu-buzut-31a867162');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                    json = (JSON.parse(xhr.responseText));
+                    dados();
+
+                } else {
+                    reject('Erro na Req.');
+                }
+            }
+        }
+    })
+}
+
+minhaPromise()
+    .then(function(response) {
+        console.log(response);
+    })
+
+    .catch(function(error) {
+        console.warn(error);
+        document.body.innerHTML = 'Erro ao carregar os dados';
+    })
+
+
+function dados(){
+
+  document.getElementById('perfil').innerHTML = `
+    <img src='${json.avatar_url}'>
+    <h2>${json.name} </h2>
+    <p>${json.bio}</p>`;
+
+}
