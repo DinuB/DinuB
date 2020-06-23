81f32cdbb1849ff90c7ab7af573f4892e960d48e
dinu.buzut.enext <dinu@dinu.buzut.enext>
2020-06-22 21:21:17 -0300

    teste

--- src/perfil.js
+++ src/perfil.js
@@ -9,7 +9,6 @@ var minhaPromise = function(){
     return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.github.com/users/dinub');
-        // xhr.open('GET', 'https://api.linkedin.com/v1/dinu-buzut-31a867162');
         xhr.send(null);
 
         xhr.onreadystatechange = function() {
@@ -39,15 +38,5 @@ minhaPromise()
 
 
 function dados(){
-
-  document.getElementById('perfil').innerHTML =
-  `
-  <div class='img'>
-    <img src='src/img/fotocvdinufim.png'>
-  </div>
-  <div class='bio'>
-      <h3 style='color: #fff'>DinuB: UNDER CONSTRUTION!!!</h3>
-  </div>
-    `;s
 
 }
