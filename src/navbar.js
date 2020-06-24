67dc44c051fde4eb6401dba5c67db73a819f6eaf
dinu.buzut.enext <dinu@dinu.buzut.enext>
2020-06-23 21:52:44 -0300

    new stage

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,11 @@
 
+function openMenu() {
+    if (!$("#menu").hasClass("activeMenu")) {
+        console.log("nao tem class active"); 
+        $("#menu").addClass("activeMenu");
+        $("#mobileButton").addClass("activeButton"); 
+    } else {
+        $("#menu").removeClass("activeMenu");
+        $("#mobileButton").removeClass("activeButton");
+    }
+}
