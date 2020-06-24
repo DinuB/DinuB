096710f3c1f736d116a850c7fc65021a3a9ea814
dinu.buzut.enext <dinu@dinu.buzut.enext>
2020-06-23 22:07:17 -0300

    new stage

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,25 @@
 
+// function openMenu() {
+//     if (!$("#menu").hasClass("activeMenu")) {
+//         console.log("nao tem class active"); 
+//         $("#menu").addClass("activeMenu");
+//         $("#mobileButton").addClass("activeButton"); 
+//     } else {
+//         $("#menu").removeClass("activeMenu");
+//         $("#mobileButton").removeClass("activeButton");
+//     }
+// }
+
+
+var menu = document.getElementById("menu");
+var menuButton = document.getElementById("mobileButton")
+
 function openMenu() {
-    if (!$("#menu").hasClass("activeMenu")) {
-        console.log("nao tem class active"); 
-        $("#menu").addClass("activeMenu");
-        $("#mobileButton").addClass("activeButton"); 
+    if (!menu.classList.contains("activeMenu")) {
+        menu.classList.add("activeMenu")
+        menuButton.classList.add("activeButton"); 
     } else {
-        $("#menu").removeClass("activeMenu");
-        $("#mobileButton").removeClass("activeButton");
+        menu.classList.remove("activeMenu")
+        menuButton.classList.remove("activeButton"); 
     }
 }
