4f86fcbcb14d4b54081a71de43c843989ea5463c
DinuB <dinuflow@outlook.com>
2020-12-30 23:12:15 -0300

    New Visual

---
+++ src/head.js
@@ -0,0 +1,44 @@
+// var json;
+// var submit;
+
+// function submit(){
+//   submit = document.getElementById('input').value;
+// }
+
+// var minhaPromise = function(){
+//     return new Promise(function(resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://api.github.com/users/dinub');
+//         xhr.send(null);
+
+//         xhr.onreadystatechange = function() {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     resolve(JSON.parse(xhr.responseText));
+//                     json = (JSON.parse(xhr.responseText));
+//                     dados();
+
+//                 } else {
+//                     reject('Erro na Req.');
+//                 }
+//             }
+//         }
+//     })
+// }
+
+// minhaPromise()
+//     .then(function(response) {
+//         console.log(response);
+//     })
+
+//     .catch(function(error) {
+//         console.warn(error);
+//         document.body.innerHTML = 'Erro ao carregar os dados';
+//     })
+
+
+// function dados(){
+
+// }
+
+// logo enext
